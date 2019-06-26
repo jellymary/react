a304366449503573a8b7eb9d2987a7197b9a55b6
jellymary <marykuzovleva@gmail.com>
2019-06-26 12:59:47 +0500

    Condition complete

--- tasks/src/2.3.Condition/index.js
+++ tasks/src/2.3.Condition/index.js
@@ -16,17 +16,21 @@ import './styles.css';
  */
 
 function renderPosts(posts) {
-  //<div className="emptyPosts">Нет откликов</div>
-  //<div className="singlePost">Единственный отклик</div>
-  return <div className="posts">Отклики в количестве {posts.length}</div>;
+  if (posts.length === 0) {
+    return (<div className="emptyPosts">Нет откликов</div>);
+  } else if (posts.length === 1) {
+    return (<div className="singlePost">Единственный отклик</div>);
+  } else {
+    return (<div className="posts">Отклики в количестве {posts.length}</div>);
+  }
 }
 
 function renderLot(name, description, tags) {
   return (
     <div className="lot">
-      <div className="lotName">{name}</div>
-      <div className="lotDescription">{description}</div>
-      {renderTags(tags)}
+      <div className="lotName">{name || '<Неизвестный предмет>'}</div>
+      {description && <div className="lotDescription">{description}</div>}
+      {tags.length > 0 ? renderTags(tags) : null}
     </div>
   );
 }
